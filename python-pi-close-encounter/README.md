ee44eb2e8ad51b448b387d0df3880c2bcf56ec7d
Scott Davies <scottmdavies@gmail.com>
2018-08-25 17:19:14 +0000

    updated files with comments and reviewed content

--- python-pi-close-encounter/README.md
+++ python-pi-close-encounter/README.md
@@ -1,11 +1,36 @@
-# Close Encounter of a Third Kind
+# Close Encounter
+
+Script for a RaspberryPi that sends signal to Pimorni Blinkt! LEDs and synth keys to SonicPi via python-sonic
+
+# Background
+
+## Python-Sonic
+
+Send signals to open SonicPi via OSC messages
+
+https://github.com/gkvoelkl/python-sonic 
+
+## Famous Keyboard Sequence
+
+```
+ Start with the tone. (Pinkish-red) - G, 67
+ Up a full tone. (Orange) - A, 69
+ Down a major third. (Purple) - F, 65
+ Now drop an octave. (Yellow) - F (an octave lower), 53
+ Up a perfect fifth. (White) - C, 60
+```
+http://www.johnloomis.org/ece303L/notes/music/Close_Encounters.html
+
+Used to assign Blinkt! LED colours and SonicPi synth keys
+
+
+
 
 # Required
 
 + RaspberryPi
 + SonicPi
 + Pimoroni Blinkt!
-
 
 # Python Project Setup
 
@@ -23,6 +48,10 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-python3 app.py
+python3 close_encounter.py
 
 ```
+
+# Optional
+
+Make a mountain out of mashed potato
--- python-pi-close-encounter/close_encounter.py
+++ python-pi-close-encounter/close_encounter.py
@@ -1,29 +1,26 @@
-# https://andymurkin.files.wordpress.com/2012/01/midi-int-midi-note-no-chart.jpg
-# http://www.johnloomis.org/ece303L/notes/music/Close_Encounters.html
+#!/usr/bin/env python
 
-#Start with the tone. (Pinkish-red) - G, 67
-#Up a full tone. (Orange) - A, 69
-#Down a major third. (Purple) - F, 65
-#Now drop an octave. (Yellow) - F (an octave lower), 53
-#Up a perfect fifth. (White) - C, 60
-
-# https://github.com/gkvoelkl/python-sonic 
+"""close_encounter.py: Plays sequence of notes and lights LEDs."""
 
 import time
 import psonic
 import blinkt
 
-# define colour RGB values
+# defines Blinkt RGB values for range of colours in sequence
 white = (255, 255, 255)
 orange = (255, 127, 0)
 pink = (255, 182, 193)
 purple = (128, 0, 128)
 yellow = (255, 255, 0) 
 
-# delay in seconds for LED and playing sound
+# delay in seconds for LED and playing sound, controls overall loop
 delays = [0.5, 0.25, 0.2, 0.15, 0.1]
 
-# turning on the LEDs
+# list of colours
+colours = [pink,orange,purple,yellow,white]
+
+# turns on the Blinkt LEDs with relevant colour
+
 def show_led(led_num, led_colour, delay):
 	r,g,b = led_colour
 	blinkt.clear()
@@ -32,16 +29,19 @@ def show_led(led_num, led_colour, delay):
 	blinkt.show()
 	time.sleep(d)
 
+# order of synth notes being played in the sequence
 notes = [psonic.G4,psonic.A4,psonic.F4,psonic.F3,psonic.C4]
-colours = [pink,orange,purple,yellow,white]
 
-# Mapping the 8 pixels to notes
-# FXCDEFGA
-# 01234567
+# limited by the 8 pixels so mapping synth notes to index
+```
+FXCDEFGA
+01234567
+```
 order = [6,7,5,0,2]
 
-# loop
+# overall loop
 for d in delays:
+	# iterate through notes in synth sequence
 	for i, note in enumerate(notes):
 		psonic.use_synth(psonic.SAW)
 		psonic.play(note)
