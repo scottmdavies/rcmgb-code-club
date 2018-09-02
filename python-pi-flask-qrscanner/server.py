688db14a1999b3d09831afb12be6e5e41e1c664b
Scott Davies <scottmdavies@gmail.com>
2018-09-02 10:05:36 +0000

    Basic webserver loading both pages

--- python-pi-flask-qrscanner/server.py
+++ python-pi-flask-qrscanner/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/scan/')
+def scan_qr():
+    return render_template("scan-qr.html")
+
+@app.route('/generate/')
+def generate_qr():
+    return render_template("generate-qr.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host="0.0.0.0")
